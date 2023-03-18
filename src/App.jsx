dcef3605030153ef202552ee8fd4381e08e33754
Andre Cambuy <andre25531@gmail.com>
2023-03-18 13:49:55 -0300

    Pagina Inicial do projeto

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,12 @@
 import "./App.css";
+import StartScreen from "./components/StartScreen";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <StartScreen />
+    </div>
+  );
 }
 
 export default App;
---
+++ src/components/StartScreen/index.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "./StartScreen.css";
+
+const StartScreen = () => {
+  return (
+    <div className="start">
+      <h1>Secret Word</h1>
+      <p>Clique no botão a baixo para começar a jogar</p>
+      <button>Começar o jogo</button>
+    </div>
+  );
+};
+
+export default StartScreen;
