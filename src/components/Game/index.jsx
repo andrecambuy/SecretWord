97226a5b60c5e8426d7a854915590f2befe1307e
Andre Cambuy <andre25531@gmail.com>
2023-03-19 20:16:00 -0300

    Fuções Principais do jogo

--- src/components/Game/index.jsx
+++ src/components/Game/index.jsx
@@ -1,11 +1,50 @@
 import React from "react";
 import "./Game.css";
 
-const Game = ({ verfyLetter }) => {
+const Game = ({
+  verfyLetter,
+  pickedWord,
+  pickedCategory,
+  letters,
+  guessedLetters,
+  wrongLetters,
+  guesses,
+  score,
+}) => {
   return (
-    <div>
-      <h1>Game</h1>
-      <button onClick={verfyLetter}>End</button>
+    <div className="game">
+      <p className="points">
+        <span>Pontuação: {score}</span>
+      </p>
+      <h1>Adivinhe a palavra</h1>
+      <h3 className="tip">
+        Dica sobre a palavra: <span>{pickedCategory}</span>
+      </h3>
+      <p>Voçe ainda tem {guesses} tentativas</p>
+      <div className="wordContainer">
+        {letters.map((letter, i) =>
+          guessedLetters.includes(letter) ? (
+            <span key={i} className="letter">
+              {letter}
+            </span>
+          ) : (
+            <span key={i} className="blankSquare"></span>
+          )
+        )}
+      </div>
+      <div className="letterContainer">
+        <p>Tente adivinhar uma letra da palavra</p>
+        <form>
+          <input type="text" name="letter" maxLength="1" required />
+          <button>Jogar!</button>
+        </form>
+      </div>
+      <div className="wrongLetterContainer">
+        <p>letras já utilizadas:</p>
+        {wrongLetters.map((wrongLetter, i) => (
+          <span key={i}>{wrongLetter}</span>
+        ))}
+      </div>
     </div>
   );
 };
